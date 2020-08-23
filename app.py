4a46b042d6635bacc5e3d329844a452b13c41153
wisdom osagbemeh <wisdomosagbemeh@gmail.com>
2020-08-23 11:04:47 +0100

    Initial commit
    :wq

---
+++ app.py
@@ -0,0 +1,12 @@
+age_limit = 18
+
+print('hello dear')
+print('enter your name')
+user = input()
+print('goodday', user)
+print('enter your age sir to verify if your legible for loan')
+age = input()
+if age >= str(age_limit):
+    print('mr', user + ' Congrats your legible')
+else:
+    print('sorry your legible try next year!!!')
